17ff34e02fb65c9ce01594401b4de39cbfae33af
suspended7th <donotcontactme@notreal.email>
2022-04-29 12:07:10 -0400

    extremely basic boiler plate webserver created

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # SuperHeroArena
 A quick flask application to battle superheroes with RNG and stats
+
+
+## required pip packages:
+- virtualenv
+- flask
+- flask_sqlalchemy
+- passlib
---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, redirect, request
+from flask_sqlalchemy import SQLAlchemy
+
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String(20), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+db.create_all()
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User(username=username, password=password)
+        try:
+            db.session.add(user)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "There was an issue adding the user"
+    else:
+        users = User.query.order_by(User.date_created).all()
+        return render_template('index.html', users=users)
+
+if __name__ == "__main__":
+    app.run(debug=True)
