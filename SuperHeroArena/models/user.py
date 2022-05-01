2bdc6478cdcd4c539b49603cbd446fc7e8bb41e2
suspended7th <donotcontactme@fake.email>
2022-05-01 00:07:26 -0400

    user password encryption added and login tester added to ensure its functionality.

--- SuperHeroArena/models/user.py
+++ SuperHeroArena/models/user.py
@@ -1,18 +1,23 @@
 from .. import db
 from datetime import datetime
-# from password_manager import generate_hashed_password
+from passlib.hash import bcrypt
+
+hasher = bcrypt.using(rounds=12)
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False)
     password = db.Column(db.String(20))
-    salt = db.Column(db.String[22])
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
     
-    # def __init__(self, username, password=None, date_created=None):
-    #     db.Model.__init__(self, username=username, date_created=date_created)
-    #     self.password = generate_hashed_password(self, password)
+    def __init__(self, username, password=None, date_created=None):
+        db.Model.__init__(self, username=username, date_created=date_created)
+        self.password = hasher.hash(password)
+
     
     def __repr__(self):
-        return '<Task %r>' % self.id
+        return '<User {}>'.format(self.username)
+
+    def authenticate(self, password):
+        return hasher.verify(password, self.password)
